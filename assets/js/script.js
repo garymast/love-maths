af896add295ef784c423b86495963c2055314d1d
garymast <>
2023-05-23 19:19:24 +0000

    Add code for subta=ract game

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,11 @@ function runGame(gameType) {
     else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
     }
+
+    else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    }
+
     else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting`;
@@ -61,6 +66,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -84,8 +91,10 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand2 < operand1 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
